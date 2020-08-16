ae0e9c60e40a32c6af88ed2557fc43c114051821
Emily Yu <eyudeveloper@gmail.com>
2020-08-16 11:15:51 -0700

    forgot to change title

--- SDLProject/main.cpp
+++ SDLProject/main.cpp
@@ -74,7 +74,7 @@ void DrawBackground(GLuint backgroundImage) {
 }
 void Initialize() {
     SDL_Init(SDL_INIT_VIDEO);
-    displayWindow = SDL_CreateWindow("Textured!", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_OPENGL);
+    displayWindow = SDL_CreateWindow("Hiroyuki Sawano BulletHell", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_OPENGL);
     SDL_GLContext context = SDL_GL_CreateContext(displayWindow);
     SDL_GL_MakeCurrent(displayWindow, context);
     
